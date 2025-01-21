6751a413c2af5ac15b1ac3db47c58505becc2ae4
Max Asselmeier <mass@gatech.edu>
2025-01-21 14:50:26 -0500

    created files for opencv implementation

---
+++ include/superpixels/SuperpixelColorOpenCVSegmenter.h
@@ -0,0 +1,46 @@
+#include <ros/node_handle.h>
+#include <ros/package.h>
+
+#include <superpixels/SuperpixelSegmenter.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+// Include CvBridge, Image Transport, Image msg
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <yaml-cpp/yaml.h>
+
+/**
+* \brief Base class for superpixel segmenter
+*/
+class SuperpixelColorOpenCVSegmenter : public SuperpixelSegmenter
+{
+    public:
+        SuperpixelColorOpenCVSegmenter(ros::NodeHandle nh, const std::string & config_path);
+
+        void runSegmentation();
+
+    private:
+        void runSuperpixels();
+
+        double calculateDistance();
+
+        ros::NodeHandle nh_;
+
+        cv_bridge::CvImagePtr color_image_ptr_ = nullptr;
+        cv_bridge::CvImagePtr superpixel_label_image_ptr_ = nullptr;
+        cv_bridge::CvImagePtr superpixel_distance_image_ptr_ = nullptr;
+
+        image_transport::Publisher color_image_pub_;
+
+        struct SuperpixelParams
+        {
+            int k_ = 0; // Desired number of approximately equally-sized superpixels
+        };
+
+        SuperpixelParams params_;
+
+};
---
+++ src/SuperpixelColorOpenCVSegmenter.cpp
@@ -0,0 +1,56 @@
+#include <superpixels/SuperpixelColorOpenCVSegmenter.h>
+
+SuperpixelColorOpenCVSegmenter::SuperpixelColorOpenCVSegmenter(ros::NodeHandle nh, const std::string & config_path) : SuperpixelSegmenter(nh, config_path), nh_(nh)
+{
+
+    image_transport::ImageTransport it(nh);
+
+    // Load configs
+    YAML::Node configYamlNode = YAML::LoadFile(config_path);
+
+    params_.k_ = configYamlNode["superpixels"]["k"].as<int>();
+
+    color_image_ptr_ = cv_bridge::CvImagePtr(new cv_bridge::CvImage);
+
+    // read in example image
+    const std::string superixels_path = ros::package::getPath("superpixels");
+    const std::string image_name = "flower.jpg";
+
+    color_image_ptr_->header.stamp = ros::Time::now();
+    color_image_ptr_->header.frame_id = "camera_link";
+    color_image_ptr_->header.seq = 0;
+    color_image_ptr_->encoding = sensor_msgs::image_encodings::BGR8;
+    color_image_ptr_->image = cv::imread(superixels_path + "/images/" + image_name, cv::IMREAD_COLOR);
+
+    if (color_image_ptr_->image.empty())
+    {
+        ROS_ERROR_STREAM("Could not read the image: " << image_name);
+        return;
+    }
+
+    color_image_pub_ = it.advertise("/superpixels/color", 1);
+
+    return;
+}
+
+void SuperpixelColorOpenCVSegmenter::runSegmentation()
+{
+    // Superpixels algorithm
+
+    color_image_ptr_->header.stamp = ros::Time::now();
+
+    color_image_pub_.publish(color_image_ptr_->toImageMsg());
+
+    return;
+}
+
+void SuperpixelColorOpenCVSegmenter::runSuperpixels()
+{
+
+    return;
+}
+
+double SuperpixelColorOpenCVSegmenter::calculateDistance()
+{
+    return 0.0;
+}
