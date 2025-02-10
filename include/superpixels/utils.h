e039d63c39609c703a8ff8c67e451a093e01e60f
Max Asselmeier <mass@gatech.edu>
2025-02-10 18:14:06 -0500

    tweaked some parameters, need to add some quick and dirty convex hull algo

--- include/superpixels/utils.h
+++ include/superpixels/utils.h
@@ -15,21 +15,21 @@ const float DELTA = std::numeric_limits<float>::epsilon();
 // Parameters
 struct SuperpixelParams
 {
-    int num_superpixels_ = 0; // Desired number of approximately equally-sized superpixels
+    int num_superpixels_ = 100; // Desired number of approximately equally-sized superpixels
     int step_ = 0; // superpixel grid interval
-    double w_normal_ = 1.0; // Weighting parameter for normal similarity term
-    double w_pos_ = 10.0; // Weighting parameter for plane - position distance term
+    double w_normal_ = 5.0; // Weighting parameter for normal similarity term
+    double w_pos_ = 40.0; // Weighting parameter for plane - position distance term
     double w_compact_ = 1.0; // Weighting parameter for compactness term
     int num_dilation_iterations_ = 0; // Number of dilation iterations
-    int num_iterations_ = 0; // Number of iterations
+    int num_iterations_ = 3; // Number of iterations
     bool warm_start_ = false; // Warm start
-    int kernel_radius_ = 2; // Kernel size for dilation
+    int kernel_radius_ = 0; // Kernel size for dilation
 
     int k_c_ = 512; // Floor width (pixels)
-    double v_fov_ = M_PI / 2; // Vertical field of view (radians)
+    double v_fov_ = M_PI / 3; // Vertical field of view (radians)
     double v_offset_ = 0.0;
     double h_ = (v_fov_ / 2) - v_offset_; // Vertical angle from camera to floor (radians)
-    double egocan_radius_ = 1.0; // Radius of egocylinder (meters)
+    // double egocan_radius_ = 1.0; // Radius of egocylinder (meters)
 };
 
 inline bool isPixelInBounds(const int & k_c, 
